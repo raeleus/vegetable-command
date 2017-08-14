9707c0d0d00f8e8279b324ec9e8166a58f7494de
Raymond Buckley <raymond.ray3k@gmail.com>
2017-08-13 22:49:12 -0700

    Initial project setup.

---
+++ desktop/src/com/ray3k/vegetablecommand/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.ray3k.vegetablecommand.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.ray3k.vegetablecommand.Core;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Core(), config);
+	}
+}
